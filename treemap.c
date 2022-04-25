8520e7c056dce6852c6ef8254ad1b142f0dba064
Your Name <you@example.com>
2022-04-24 23:23:28 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -285,26 +285,35 @@ Pair * nextTreeMap(TreeMap * tree) {
     else
     {
         printf("No tiene R\n");
+        if(tree->current->right != NULL)
+    {
+        //printf("Tiene R\n");
+        tree->current = minimum(tree->current->right);
+    }
+    else
+    {
+        //printf("No tiene R\n");
         if(tree->current->parent != NULL)
         {
             TreeNode * aux = tree->current->parent;
 
             while(1)
             {
-                printf("O");
+                //printf("O");
                 if(aux != NULL)
                 {
-                    printf("\nTiene padre\n");
+                    //printf("\nTiene padre\n");
                     if(tree->lower_than(tree->current->pair->key, aux->pair->key) == 1)
                     {
                         break;
                     }
-                    printf("Current > aux\n");
+                    //printf("Current > aux\n");
                     aux = aux->parent;
                 }
                 else
                 {
-                    printf("\nNo tiene padre\n");
+                    //printf("\nNo tiene padre\n");
+                    printf("xd\n");
                     return NULL;
                 }
             }
@@ -312,8 +321,10 @@ Pair * nextTreeMap(TreeMap * tree) {
             }
         else
         {
+            printf("xd2");
             return NULL;
         }
     }
+    printf("xd3");
     return tree->current->pair;
 }
